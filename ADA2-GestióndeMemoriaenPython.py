6ad672a9b990e57c36305940b4c680c4d3bbf266
orzaboss <e24081066@itmerida.edu.mx>
2025-09-07 06:37:52 -0600

    ADA2- Gestión de Memoria en Python Orlando Chan

---
+++ ADA2-GestióndeMemoriaenPython.py
@@ -0,0 +1,32 @@
+#Lista estática
+calificaciones = [0] * 5
+
+for i in range(5):
+    calificaciones[i] = int(input(f"Captura la calificación {i + 1}: "))
+
+
+print("\nCalificaciones ingresadas:")
+for i, cal in enumerate(calificaciones, start=1):
+    print(f"Calificación {i}: {cal}")
+
+print("-------------------")
+
+# Lista dinámica 
+frutas = []  
+
+
+frutas.append("Mango")
+frutas.append("Manzana")
+frutas.append("Banana")
+frutas.append("Uvas")
+
+print(frutas)
+
+
+frutas.pop(0)  
+frutas.pop(1)  
+
+
+frutas.append("Sandía")
+
+print(frutas)
